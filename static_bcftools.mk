6b4453d74d2164794e52a140bd34f552b56e8f50
Dr. K.D. Murray <foss@kdmurray.id.au>
2024-11-22 10:51:48 +0100

    bcftools: don't use /usr/local/lib for plugin path

--- static_bcftools.mk
+++ static_bcftools.mk
@@ -1,6 +1,7 @@
 USE_GSL=1
 include Makefile
 
+vcfplugin.o: EXTRA_CPPFLAGS =
 static_bcftools: $(OBJS) plugins
 	$(CC) $(DYNAMIC_FLAGS) $(ALL_LDFLAGS) -o bcftools $(OBJS) -l:libgsl.a -l:libgslcblas.a -l:libhts.a -l:libdeflate.a -l:libbz2.a -l:liblzma.a  -l:libz.a -lm -ldl -lpthread
 
